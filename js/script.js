2e91c78a925bb7a84c9802504f00f17f4ad33dd2
halima254 <halima.yahya@student.moringaschool.com>
2021-08-02 08:04:02 +0300

    assign prices to pizza flavour, size and crust type

--- js/script.js
+++ js/script.js
@@ -12,3 +12,51 @@ function submitOrder(){
    
    );
 }
+var sizePrice;
+    if (flavour === "Barbequed" || flavour==="Bufallo"||flavour==="Margheritta"){
+        if(size==="small"){
+            sizePrice = 800;
+        }
+        else if(size === "medium"){
+            sizePrice = 1000;
+        }
+        else if(size==="large"){
+            sizePrice=1200;
+        }
+    }
+    else if (flavour === "Meat" || flavour==="Pepperoni"||flavour==="Vegetable"){
+        if(size==="small"){
+            sizePrice = 900;
+        }
+        else if(size === "medium"){
+            sizePrice = 1100;
+        }
+        else if(size==="large"){
+            sizePrice=1300;
+        }
+    }
+     else if  (flavour === "cheesy" || flavour==="Hawaiian"){
+        if(size==="small"){
+            sizePrice = 1000;
+        }
+        else if(size === "medium"){
+            sizePrice = 1200;
+        }
+        else if(size === "large"){
+            sizePrice = 1400;
+        }
+    }
+    var crustType;
+    if (crust === "stuffed"){
+            crustType= 250;
+    }
+        else if(crust === "cheesy"){
+            crustType = 350;
+        }
+        else if(crust==="crispy"){
+            sizePrice=200;
+        }
+        else if(crust==="gluten-free"){
+            sizePrice=400;
+        }
+    
